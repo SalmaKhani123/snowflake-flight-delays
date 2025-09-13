74127b4ca4e55c68706e1c220cadc8e848bc5330
SalmaKhani123 <salkhani2003@gmail.com>
2025-09-12 22:42:11 -0500

    Set up initial database and schema for flight_project

---
+++ sql/01_workspace_setup.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE DATABASE flight_project;
+CREATE OR REPLACE SCHEMA  flight_project.flight_project_raw;
+CREATE OR REPLACE STAGE   flight_project.flight_project_raw.flight_stage;
+
+USE DATABASE flight_project;
+USE SCHEMA   flight_project_raw;
